ae161b1f3115a3c8f6bb88ad6877922e0579bf44
Karabo Mashiho <100083278+MicKnights@users.noreply.github.com>
2022-02-24 07:33:48 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main{
+    public static void main(String[] args){
+        HelloWorld hello_world = new HelloWorld();
+        hello_world.runHello();
+
+        // runHello();
+    }
+}
